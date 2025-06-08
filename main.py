996c19b43e430bd8740194360761435acf7f99d7
mediamanipulation <manipulation2000@hotmail.com>
2025-06-08 12:15:32 -0500

    fixing folder structure

--- main.py
+++
@@ -1,9 +0,0 @@
-# main.py
-
-import tkinter as tk
-from gui.app import ExcelTransformerApp
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = ExcelTransformerApp(root)
-    root.mainloop()
